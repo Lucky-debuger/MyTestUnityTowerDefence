7dd86cb49ccbe522534b259ad201d5e4f9f19f09
Lucky-debuger <79850821+Lucky-debuger@users.noreply.github.com>
2025-07-28 02:26:56 +0300

    Continue work with HUD

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -5,6 +5,13 @@ public class Billboard : MonoBehaviour
 
     public Transform cam;
     // Update is called once per frame
+    void Awake()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main.transform;
+        }
+    }
     void LateUpdate()
     {
         transform.LookAt(transform.position + cam.forward);
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Enemy : MonoBehaviour
 {
+    public HealthBar healthBar;
     [SerializeField] private int _maxHealth = 100;
     private int _currentHealth;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _currentHealth = _maxHealth;
+        healthBar.SetMaxHealth(_maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    
+    public void TakeDamage(InputAction.CallbackContext context)
+    {
+        // Debug.Log("Current phase:" + context.phase);
+        if (context.started)
+        {
+            _currentHealth -= 20;
+            healthBar.SetHealth(_currentHealth);
+        }
+
     }
 }
