06a90dadf2eda8ead479cd3aef4390ba5657778f
Lucky-debuger <79850821+Lucky-debuger@users.noreply.github.com>
2025-08-24 14:15:44 +0300

    Cuntinue develop buildZOne

--- Assets/Scripts/BuildZone/BuildZoneVisual.cs
+++ Assets/Scripts/BuildZone/BuildZoneVisual.cs
@@ -7,22 +7,15 @@ public class BuildZoneVisual : MonoBehaviour
     
     private MeshRenderer zoneRenderer;
     private BoxCollider zoneCollider;
+    private MeshFilter zoneMeshFilter;
+
 
     void Start()
     {
         zoneCollider = GetComponent<BoxCollider>();
-        zoneRenderer = gameObject.AddComponent<MeshRenderer>();
-        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
-
-        // Используем встроенный меш куба
-        meshFilter.sharedMesh = Resources.GetBuiltinResource<Mesh>("Cube.fbx");
-
-        // Загружаем заранее созданный материал
-        Material zoneMaterial = Resources.Load<Material>("BuildZoneMaterial");
-        zoneRenderer.material = zoneMaterial;
-
-        // Масштабируем под размер коллайдера
-        transform.localScale = zoneCollider.size;
-
+        zoneMeshFilter = GetComponent<MeshFilter>();
+        zoneRenderer = GetComponent<MeshRenderer>();
     }
+
+
 }
