1071e95743c89720d344a15d2a3a71a0e2dd3711
unknown <lesha-butenko1@mail.ru>
2025-11-26 16:26:13 +0300

    None

--- Assets/Scripts/Turrets/LaserBeamer.cs
+++ Assets/Scripts/Turrets/LaserBeamer.cs
@@ -10,19 +10,31 @@ public class LaserBeamer : Tower
     [Header("Use laser")]
     public bool useLaser = false;
     public LineRenderer lineRenderer;
+    [SerializeField] ParticleSystem impactEffect;
     protected override void Attack()
     {
         lineRenderer.SetPosition(0, _projectileSpawnPoint.position);
         lineRenderer.SetPosition(1, _currentTarget.position);
-        Debug.Log(Time.time.ToString() + " attack!");
     }
 
     protected override void RotateHead()
     {
-        if (_currentTarget == null) return;
+        if (_currentTarget == null)
+        {
+            lineRenderer.enabled = false;
+            impactEffect.Stop();
+            return;
+        }
+        else
+        {
+            lineRenderer.enabled = true;
+            impactEffect.Play();
+        }
 
         GameObject partToRotate = transform.Find("PartToRotate").gameObject;
         Vector3 direction = _currentTarget.transform.position - partToRotate.transform.position;
         partToRotate.transform.rotation = Quaternion.Slerp(partToRotate.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotationSpeed);
+        impactEffect.transform.position = _currentTarget.transform.position;
+        Debug.Log(impactEffect.transform.position);
     }
 }
